626b99aebea8eaddd7662d2b9772e799a86b9444
Naupaka Zimmerman <naupaka@gmail.com>
2014-10-01 15:14:48 -0700

    fix script and add comments

--- parse_metadata.py
+++ parse_metadata.py
@@ -1,9 +1,35 @@
+#!/usr/bin/python
+
+###################################################################
+# Script to parse GenBank metadata into csv format, accounting for
+# missing or incomplete data
+# Written October 1, 2014 by Naupaka Zimmerman, naupaka@gmail.com
+# Call with:
+# print 'python parse_metadata.py <inputfile> <outputfile>'
+###################################################################
+
+import sys
 import re
-print 2 + 2
-in_file = open("Blast_accession98pct.meta2.txt")
-out_file = open("out.csv", "a")
+
+# Set to True to print some additional debug information
+DEBUG = False
+
+# Read in parameters
+if len(sys.argv) == 3:
+    print 'Input file is: ', sys.argv[1]
+    print 'Output file is: ', sys.argv[2]
+    inputfile, outputfile = sys.argv[1], sys.argv[2]
+else:
+    print 'Two parameters needed, you provided ' + len(sys.argv)
+    print 'python parse_metadata.py <inputfile> <outputfile>'
+    sys.exit()
+
+# open input file, with output file in append mode
+in_file = open(inputfile)
+out_file = open(outputfile, "a")
 features = []
 
+# find non-redundant list of feature types over the whole file
 for line in in_file:
     if re.search(r"^\t", line):
         feature_type = re.search(r"^\t(\S.*?)\t", line)
@@ -13,14 +39,28 @@ for line in in_file:
 
 in_file.close()
 
-out_file.write('"SEQ_ID", "REF_TITLE", "' + '", "'.join(features) + '"\n')
+if DEBUG:
+    print features[0]
 
-in_file = open("Blast_accession98pct.meta2.txt")
-out_line = ["", ""]
+# Write column headers
+out_file.write('"SEQ_ID","REF_TITLE","' + '","'.join(features) + '"\n')
+
+# reopen input file to capture data
+in_file = open(inputfile)
+
+# Create empty line to store results (length based on num of features + 2)
+out_line = ['', '']
+out_line.extend([''] * len(features))
+
+# set count to avoid writing on 1st line before content is parsed
 count = 0
 
+if DEBUG:
+    print out_line
+
+# Iterate over lines, adding to existing columns if features are present
+# Add newline when reaching a new sequence ID
 for line in in_file:
-    out_line.extend([""] * len(features))
     if re.search(r"^(\S*)\n", line):
         if count == 0:
             count += 1
@@ -28,8 +68,9 @@ for line in in_file:
             out_line[0] = item.group(1)
         elif count != 0:
             count += 1
-            out_file.write('","'.join(out_line) + '"\n')
-            out_line = []
+            out_file.write('"' + '","'.join(out_line) + '"\n')
+            out_line = ["", ""]
+            out_line.extend([""] * len(features))
             item = re.search(r"^(\S*)\n", line)
             out_line[0] = item.group(1)
     elif re.search(r"^\sREF\sTITLE:\s(.*)\n", line):
@@ -38,16 +79,10 @@ for line in in_file:
     elif re.search(r"^\t(\S*?)\t(.*)\n", line):
         item = re.search(r"^\t(\S*?)\t(.*)\n", line)
         out_line[features.index(item.group(1)) + 2] = item.group(2)
+    elif re.search(r"^\sFeatures:\n", line):
+        continue
     else:
         print "Error: " + line + "\n"
 
 in_file.close()
 out_file.close()
-
-print features
-print out_line
-re.search(r"^(\S)\n", line)
-
-print features
-# len(features)
-# len(out_line)
